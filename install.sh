97600b6d73716d15e7acab4eb4e2c82cec5f061b
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2016-01-11 10:18:31 -0600

    kde settings

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ cp ./tmux/.tmux.conf ~/.tmux.conf
 cp ./vim/.vimrc ~/
 cp -rf ./vim/colors ~/.vim
 cp ./kde4/share/config/* ~/.kde4/share/config
+cp ./kde4/share/apps/konsole/* ~/.kde4/share/apps/konsole
