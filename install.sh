fd69e5324f9266b9178d3ff5a3a4b81f54907eff
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2016-08-10 15:17:23 -0600

    sublime text preferences

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 [ -d ~/.moc ] || mkdir ~/.moc
 [ -d ~/.vim/colors ] || mkdir -p ~/.vim/colors
+[ -d ~/.config/sublime-text-3/Packages/User ] || mkdir -p ~/.config/sublime-text-3/Packages/User
 cp ./moc/config ~/.moc/
 cp ./git/.gitconfig ~/
 cp ./tmux/.tmux.conf ~/.tmux.conf
 cp ./vim/.vimrc ~/
 cp -rf ./vim/colors ~/.vim
+cp -rf ./sublime_text_3/Packages/User/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User
+
 # alias
 cat .alias >> ~/.alias
